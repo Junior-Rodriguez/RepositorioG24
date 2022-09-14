3f546864ce14798578e935c9acd81f66fbd03660
Junior Rodriguez <rodrijunior1518@gmail.com>
2022-09-14 17:01:16 -0500

    Agregando clase Ejercicio2.java

---
+++ src/main/java/com/ciclo3/proyectog24/Ejercicio2.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ciclo3.proyectog24;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Ejercicio2 {
+    public void metodo(){
+    System.out.println("logica ejercicio2");
+  }
+}
