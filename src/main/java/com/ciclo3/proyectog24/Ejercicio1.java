2141f0a5e6e62c4ec1e99102de7a53c9dbe0c624
Junior Rodriguez <rodrijunior1518@gmail.com>
2022-09-15 14:45:29 -0500

    agregando funcionalidad en backend

--- src/main/java/com/ciclo3/proyectog24/Ejercicio1.java
+++ src/main/java/com/ciclo3/proyectog24/Ejercicio1.java
@@ -11,7 +11,7 @@ package com.ciclo3.proyectog24;
 public class Ejercicio1 {
    public void metodo(){
        System.out.println("Logica metodo");
-               
+       System.out.println("Agregando Logica");        
    }
     
 }
